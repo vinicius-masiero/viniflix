ebf5e0a194d523d5d824e80e54c86feff76713c7
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-07-27 15:42:53 -0300

    Updates

---
+++ precache-manifest.0603a5913c0076c4362694cec75d0d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d4b1ff8d70842eaff382fed753cebe",
+    "url": "/viniflix/index.html"
+  },
+  {
+    "revision": "de148d7d732acfa770c3",
+    "url": "/viniflix/static/css/main.76a535b5.chunk.css"
+  },
+  {
+    "revision": "178a5492d14e7e0f11b5",
+    "url": "/viniflix/static/js/2.cff2fe80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/viniflix/static/js/2.cff2fe80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de148d7d732acfa770c3",
+    "url": "/viniflix/static/js/main.3a5a8145.chunk.js"
+  },
+  {
+    "revision": "ef1d5f72d54836c3a5aa",
+    "url": "/viniflix/static/js/runtime-main.cdd4fe46.js"
+  },
+  {
+    "revision": "978a6056cbb8ec7b331851b7be60d83d",
+    "url": "/viniflix/static/media/logo.978a6056.png"
+  }
+]);
