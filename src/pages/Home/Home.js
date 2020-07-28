8ee09a8c3647e7b3082d773c2c53fc85bbea104d
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-07-28 13:18:31 -0300

    Fixed home page not covering all screen height.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import Menu from '../../components/Menu/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain/BannerMain';
 import Carousel from '../../components/Carousel/Carousel';
-import Footer from '../../components/Footer/Footer';
+import Parent from '../../components/Parent/Parent';
 
 function Home() {
   return (
-    <div style={{background: '#141414'}}>
-      <Menu />
+    <Parent>
       <Carousel category={dadosIniciais.categorias[3]} />
       <Carousel category={dadosIniciais.categorias[4]} />
-      <Footer />
-    </div>
+    </Parent>
   );
 }
 
