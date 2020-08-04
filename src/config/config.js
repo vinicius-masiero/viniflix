cbf9a15dfc53996bfdd023d707ece7d1eb6e72a0
Vinícius Masiero <vinicius.masiero@outlook.com>
2020-08-03 22:47:41 -0300

    Updated back end url.

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
-const URL_BACKEND = window.location.hostname.includes('localhost') ? 'http://localhost:8080' : 'https://viniflix.herokuapp.com';
+// const URL_BACKEND = window.location.hostname.includes('localhost') ? 'http://localhost:8080' : 'https://viniflix.herokuapp.com';
+const URL_BACKEND = 'https://viniflix.herokuapp.com';
 
 export default {URL_BACKEND};
